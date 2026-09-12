abf2fd9e10d36cb840c9c963750864beb6f215ed
BrenoRCBrito <brenorcbrito@gmail.com>
2026-09-12 15:50:45 -0300

    Remove dependências sem uso, alinha o SDK e ativa o modo estrito do tsc

--- App.tsx
+++ App.tsx
@@ -1,18 +1,13 @@
-import React  from 'react';
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { SQLiteProvider } from 'expo-sqlite';
 
 import { AppNavigator } from './src/navigation/AppNavigator';
-import { initDatabase } from './src/services/database';
 
 export default function App() {
-
   return (
-      <SQLiteProvider databaseName="assina_aqui.db" onInit={initDatabase}>
-        <StatusBar style="auto" />
-        <AppNavigator />
-      </SQLiteProvider>
+    <>
+      <StatusBar style="auto" />
+      <AppNavigator />
+    </>
   );
 }
-
-
