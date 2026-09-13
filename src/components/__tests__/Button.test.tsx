e2eb462ba377138b56e209a8805496cf3890b54d
BrenoRCBrito <brenorcbrito@gmail.com>
2026-09-13 17:25:13 -0300

    Desconta a borda do botão com contorno em qualquer tamanho

---
+++ src/components/__tests__/Button.test.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { render, screen } from '@testing-library/react-native';
+import { StyleSheet } from 'react-native';
+
+import { SettingsProvider } from '../../storage/SettingsProvider';
+import { tokens } from '../../theme';
+import { Button } from '../Button';
+
+jest.mock('@react-native-async-storage/async-storage', () =>
+  require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
+);
+
+describe('Button', () => {
+  beforeEach(async () => {
+    await AsyncStorage.clear();
+  });
+
+  test('o contorno desconta a borda do espaçamento vertical de qualquer tamanho', async () => {
+    await render(<Button label="Sair" onPress={() => undefined} preset="secondary" size="sm" />, {
+      wrapper: SettingsProvider,
+    });
+
+    const estilo = StyleSheet.flatten((await screen.findByRole('button')).props.style);
+
+    expect([estilo.paddingVertical, estilo.borderWidth]).toEqual([
+      tokens.inset.dangerY - tokens.lineWidth.outline,
+      tokens.lineWidth.outline,
+    ]);
+  });
+});
